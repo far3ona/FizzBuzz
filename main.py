52844dffabc2797bf1af63430709d68ec379f64a
Farzona <83286710+far3ona@users.noreply.github.com>
2022-06-05 21:43:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+for fizzbuzz in range(101):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print('FizzBuzz')
+    elif fizzbuzz % 3 == 0:
+        print('Fizz')
+    elif fizzbuzz % 5 == 0:
+        print('Buzz')
+    else:
+        print(fizzbuzz)
